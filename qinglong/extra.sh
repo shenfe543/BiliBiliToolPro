2d8eaa996566917cafbd534d9a10d96f03ab42e4
shenfe543 <91723935+shenfe543@users.noreply.github.com>
2024-02-26 20:26:26 +0800

    Update extra.sh

--- qinglong/extra.sh
+++ qinglong/extra.sh
@@ -3,6 +3,13 @@
 ## 安装python依赖使用 pip3 install xxx
 
 # 安装 dotnet 环境
-# dotnet --version || (curl -sSL https://ghproxy.com/https://raw.githubusercontent.com/RayWangQvQ/BiliBiliToolPro/main/qinglong/ray-dotnet-install.sh | bash /dev/stdin --no-official) && (echo "已安装dotnet")
-dotnet --version || (curl -sSL https://ghproxy.com/https://raw.githubusercontent.com/RayWangQvQ/BiliBiliToolPro/main/qinglong/ray-dotnet-install.sh | bash /dev/stdin) && (echo "已安装dotnet")
-# 其他代码...
+echo -e "\n-------set up dot net env-------"
+apk add bash icu-libs krb5-libs libgcc libintl libssl1.1 libstdc++ zlib
+wget https://download.visualstudio.microsoft.com/download/pr/3b18aefa-0e14-4193-a167-35e1de4cfe46/acf2b230ae3ecfbc4d5f4c20cbf97b2d/dotnet-sdk-6.0.419-linux-arm64.tar.gz
+DOTNET_FILE=dotnet-sdk-6.0.419-linux-arm64.tar.gz
+export DOTNET_ROOT=/home/dotnet
+mkdir -p "$DOTNET_ROOT" && tar zxf "$DOTNET_FILE" -C "$DOTNET_ROOT"
+export PATH=$PATH:$DOTNET_ROOT
+ln -s /home/dotnet/dotnet /usr/local/bin
+dotnet --version
+echo -e "\n-------set up dot net env finish-------"
